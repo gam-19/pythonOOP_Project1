b89eaa47e51d82f42af7fee7de28ba1851d79149
Ariel Morales <gonzalo.ariel.morales@outlook.com>
2023-10-25 08:58:21 -0400

    Post module completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from user import User
+from post import Post
 
 app_user_one = User("nn@nn.com", "Nana Janashia", "pwd1", "DevOps Engineer")
 app_user_one.get_user_info()
@@ -8,3 +9,6 @@ app_user_two.get_user_info()
 
 app_user_one.change_job_title("DevOps trainer")
 app_user_one.get_user_info()
+
+new_post = Post("On a secret missiong", app_user_two.name)
+new_post.get_post_info()
